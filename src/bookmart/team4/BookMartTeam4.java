42fc5c8d8612acc99a95be1d7dbe4dc2c79cfa44
Kalfin2000 <steven.e.benedict@gmail.com>
2016-03-17 11:26:11 -0500

    worked main
    started to work on main login system, it will be worked further for more
    functionality

--- src/bookmart/team4/BookMartTeam4.java
+++ src/bookmart/team4/BookMartTeam4.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package bookmart.team4;
+import java.util.Scanner;
 
 /**
  *
@@ -15,7 +16,26 @@ public class BookMartTeam4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner scanner = new Scanner(System.in);
+        
+        int menuChoice;
+        
+        System.out.println("Welcome to Bookmart, please select an option. \n"
+                + "1) Login \n"
+                + "2) Create new Account \n");
+       menuChoice = scanner.nextInt();
+       
+       while (menuChoice != 1 && menuChoice != 2)
+       {
+           System.out.println(menuChoice + " is not a valid menu selection. \n"
+                   + "Enter 1 to1 Login to and existing account. \n"
+                   + "Enter 2 to create a new account.");
+           menuChoice = scanner.nextInt();
+       }
+       
+       
+               
+        
     }
     
 }
